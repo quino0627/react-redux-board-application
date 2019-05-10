6c3ef5cacd6e50c5565beb6e126e45c3d646e1fb
quino0627 <quino0627@korea.ac.kr>
2019-05-11 03:41:38 +0900

    bug

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
   console.log("PRODUCTION");
