0bad672aa6bd4a13fc9890ba57bbcf226b06fbe2
quino0627 <quino0627@korea.ac.kr>
2019-05-10 21:51:07 +0900

    bug

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
+  console.log("PRODUCTION");
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
