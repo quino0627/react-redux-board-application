ec7c4991f9cb959afb3834d59a756a36f854784c
quino0627 <quino0627@korea.ac.kr>
2019-05-18 22:13:17 +0900

    bug

---
+++ client/src/components/editor/EditorPane/EditorPane.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import styles from "./EditorPane.scss";
+import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
+
+const cx = classNames.bind(styles);
+
+class EditorPane extends Component {
+  render() {
+    return (
+      <div className={cx("editor-pane")}>
+        <input
+          className={cx("title")}
+          placeholder="제목을 입력하세요"
+          name="title"
+        />
+        <div className={cx("code-editor")} />
+        <div className={cx("tags")}>
+          <div className={cx("description")}>태그</div>
+          <input name="tags" placeholder="태그를 입력하세요 (쉼표로 구분)" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EditorPane;
--- client/src/components/editor/EditorTemplate/EditorTemplate.js
+++ client/src/components/editor/EditorTemplate/EditorTemplate.js
@@ -6,15 +6,60 @@ import { Link } from "react-router-dom";
 const cx = classNames.bind(styles);
 
 class EditorTemplate extends Component {
+  state = {
+    leftPercentage: 0.5
+  };
+
+  handleMouseMove = e => {
+    console.log("MOUSEMOVE");
+    this.setState({
+      leftPercentage: e.clientX / window.innerWidth
+    });
+  };
+
+  handleMouseUp = e => {
+    console.log("MOPUSEUIP");
+    document.body.removeEventListener("mousemove", this.handleMouseMove);
+    window.removeEventListener("mouseup", this.handleMouseUp);
+  };
+
+  handleSeparatorMouseDown = e => {
+    console.log("handleseparatorMouseDown");
+    document.body.addEventListener("mousemove", this.handleMouseMove);
+    window.addEventListener("mouseup", this.handleMouseUp);
+  };
+
   render() {
     const { header, editor, preview } = this.props;
+    const { leftPercentage } = this.state;
+    const { handleSeparatorMouseDown } = this;
 
+    const leftStyle = {
+      flex: leftPercentage
+    };
+
+    const rightStyle = {
+      flex: 1 - leftPercentage
+    };
+
+    const separatorStyle = {
+      left: `${leftPercentage * 100}%`
+    };
     return (
-      <div className={cx("editor-template'")}>
+      <div className={cx("editor-template")}>
         {header}
         <div className={cx("panes")}>
-          <div className={cx("pane", "editor")}>{editor}</div>
-          <div className={cx("pane", "preview")}>{preview}</div>
+          <div className={cx("pane", "editor")} style={leftStyle}>
+            {editor}
+          </div>
+          <div className={cx("pane", "preview")} style={rightStyle}>
+            {preview}
+          </div>
+          <div
+            className={cx("separator")}
+            style={separatorStyle}
+            onMouseDown={handleSeparatorMouseDown}
+          />
         </div>
       </div>
     );
